3905f93d4f26f000ced334c41efb8ec123253ddb
bo.jiang <bo.jiang@finisar.com>
2017-04-19 15:05:19 +0800

    add a react-bootstrap table

--- client/main.jsx
+++ client/main.jsx
@@ -6,8 +6,10 @@ import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.jsx';
 import CountView from '../imports/ui/StoreView.jsx';
 import ProjectView from '../imports/ui/ProjectView.jsx';
+import TableSample from '../imports/ui/TableSample.jsx';
 
 Meteor.startup(() => {
+  render(<TableSample />, document.getElementById('render-table'));
   render(<ProjectView />, document.getElementById('render-project'));
   render(<CountView />, document.getElementById('render-count'));
   render(<App />, document.getElementById('render-target'));
